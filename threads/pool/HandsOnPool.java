161c5c6e65d7ce070e926aae1aa2eae4f5f119a7
Fagner Candido <fsouzacandido@gmail.com>
2020-02-27 13:15:58 +0000

    added examples to future and callable

--- threads/pool/HandsOnPool.java
+++ threads/pool/HandsOnPool.java
@@ -1,11 +1,30 @@
 package threads.pool;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 /*
 */
 
 public class HandsOnPool {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newFixedThreadPool(5);
+        Future future = es.submit(new Callable<String>() {
+
+			@Override
+			public String call() throws Exception {
+                System.out.println("Executando o pipoco");
+				return "Pipoco do Trovão";
+			}
+        });
+
+        System.out.println(""+future.get());
+
+        es.shutdown();
 
     }
 }
