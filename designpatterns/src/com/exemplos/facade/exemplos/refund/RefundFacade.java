462f4d39fab74613a0e99d4a5dd42d5ae4495a4d
Fagner Candido <fsouzacandido@gmail.com>
2020-04-13 15:27:54 +0100

    added one more refactoration to refund

--- designpatterns/src/com/exemplos/facade/exemplos/refund/RefundFacade.java
+++ designpatterns/src/com/exemplos/facade/exemplos/refund/RefundFacade.java
@@ -13,41 +13,59 @@ public class RefundFacade {
 
 class Refund {
 
-    public void registrarLog() {
-        System.out.println("registrando log");
-    }
 
-    public boolean validarCredenciais() {
-        System.out.println("validando credenciais");
-        return true;
-    }
+}
 
-    public Object obtendoUtilizador() {
-        System.out.println("obtendo utilizador");
-        return new Object();
-    }
-
-    public Object obtendoTransacao() {
-        System.out.println("obtendo transacao");
-        return new Object();
-    }
-
-    public Object obtendoTerminalDevolucao() {
-        System.out.println("obtendo terminal devolução");
-        return new Object();
-    }
-
-    public void registrarInicioRefund() {
-        System.out.println("registrando inicio refund");
-    }
-
-    public void registrarFimRefund() {
-        System.out.println("registrando fim refund");
-    }
-
+class Devolucao {
     public Object realizarDevolucao() {
         System.out.println("realizando devolucao");
         return new Object();
     }
+}
 
+class FimDevolucao {
+    public void registrarFimRefund() {
+        System.out.println("registrando fim refund");
+    }
+
+}
+
+class InicioDevolucao {
+    public void registrarInicioRefund() {
+        System.out.println("registrando inicio refund");
+    }
+}
+
+class TerminalDevolucao {
+    public Object obtendoTerminalDevolucao() {
+        System.out.println("obtendo terminal devolução");
+        return new Object();
+    }
+}
+
+class Transacao {
+    public Object obtendoTransacao() {
+        System.out.println("obtendo transacao");
+        return new Object();
+    }
+}
+
+class Utilizador {
+    public Object obtendoUtilizador() {
+        System.out.println("obtendo utilizador");
+        return new Object();
+    }
+}
+
+class Log {
+    public void registrarLog() {
+        System.out.println("registrando log");
+    }
+}
+
+class Credenciais {
+    public boolean validarCredenciais() {
+        System.out.println("validando credenciais");
+        return true;
+    }
 }
