0fec131c0f04a49afc2d7c69dadfa470bfd1a072
Marcelo Massao Kataoka Higaskino <massaohigaskino@gmail.com>
2020-04-20 21:08:09 +0100

    Implemented Singleton pattern exercise.

--- designpatterns/src/alldesigntogether/src/main/java/com/designsall/alldesigntogether/controller/InformacoesSistema.java
+++ designpatterns/src/alldesigntogether/src/main/java/com/designsall/alldesigntogether/controller/InformacoesSistema.java
@@ -4,9 +4,9 @@ import com.designsall.alldesigntogether.dto.InformacoesDTO;
 
 public class InformacoesSistema {
 
-    public InformacoesDTO status() {
-
-        InformacoesDTO dto = new InformacoesDTO();
+    private static final InformacoesDTO dto;
+    static {
+        dto = new InformacoesDTO();
         dto.setFileEncoding(System.getProperty("file.encoding"));
         dto.setJavaHome(System.getProperty("java.home"));
         dto.setJavaRuntimeName(System.getProperty("java.runtime.name"));
@@ -15,7 +15,9 @@ public class InformacoesSistema {
         dto.setOsArch(System.getProperty("os.arch"));
         dto.setOsName(System.getProperty("os.name"));
         dto.setOsVersion(System.getProperty("os.version"));
+    }
 
+    public InformacoesDTO status() {
         return dto;
     }
 
