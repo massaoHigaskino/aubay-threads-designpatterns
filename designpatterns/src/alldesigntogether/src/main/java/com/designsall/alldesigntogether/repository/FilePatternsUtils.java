22400181b7645601634166ccbcdb8ceb4f075cdc
Marcelo Massao Kataoka Higaskino <massaohigaskino@gmail.com>
2020-04-20 21:35:05 +0100

    Implemented Decorator pattern exercise.

--- designpatterns/src/alldesigntogether/src/main/java/com/designsall/alldesigntogether/repository/FilePatternsUtils.java
+++ designpatterns/src/alldesigntogether/src/main/java/com/designsall/alldesigntogether/repository/FilePatternsUtils.java
@@ -2,14 +2,24 @@ package com.designsall.alldesigntogether.repository;
 
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.nio.CharBuffer;
 
 public class FilePatternsUtils {
 
     public static String getFile() throws IOException {
-        InputStream resource = new ClassPathResource("static/test.txt").getInputStream();
-        return null;
+        StringBuilder read = new StringBuilder(10);
+        try (InputStream resource = new ClassPathResource("static/test.txt").getInputStream()) {
+            try (InputStreamReader inputStreamReader = new InputStreamReader(resource)) {
+                try (BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        read.append(line).append('\n');
+                    }
+                }
+            }
+        }
+        return read.toString();
     }
 
 }
